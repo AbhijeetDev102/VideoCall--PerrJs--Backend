d90f124300c14b0705708d0df1fa3c25c2da7716
Abhijeet Das <130581860+AbhijeetDev102@users.noreply.github.com>
2025-01-09 01:27:36 +0530

    port change

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const peerServer = ExpressPeerServer(server, {
 
 app.use("/peerjs", peerServer);
 
-server.listen(9000, ()=>{
+server.listen(8000, ()=>{
 
-	console.log("Server is running on port 9000");
+	console.log("Server is running on port 8000");
 });
