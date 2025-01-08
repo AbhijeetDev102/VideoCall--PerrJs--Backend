3c26a82049843a59e50996f4094b162e78fd8508
Abhijeet Das <130581860+AbhijeetDev102@users.noreply.github.com>
2025-01-09 01:17:26 +0530

    peer ja backend server

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { ExpressPeerServer } from "peer";
+import http from "http";
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+import cors from 'cors';
+app.use(cors(
+{
+    origin: `${process.env.FRONTEND_URL}`,
+}
+));
+
+const server = http.createServer(app);
+const customGenerationFunction = () =>
+	(Math.random().toString(36) + "0000000000000000000").substring(2, 18);
+
+const peerServer = ExpressPeerServer(server, {
+	debug: true,
+	path: "/myapp",
+    generateClientId: customGenerationFunction,
+});
+
+app.use("/peerjs", peerServer);
+
+server.listen(9000, ()=>{
+
+	console.log("Server is running on port 9000");
+});
